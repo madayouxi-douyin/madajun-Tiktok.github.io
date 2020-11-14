578f9ceabb510acc35b9785efdd91df94422f459
madajun-Tiktok <70398493+madayouxi-douyin@users.noreply.github.com>
2020-11-15 03:29:34 +0800

    x

--- ga/js/installed.js
+++
@@ -1,5 +0,0 @@
-function getJvav() {
-    document.getElementById("tou_block").style.display="none";
-    document.getElementById("verify_block").style.display="none";
-    document.getElementById("jvav_block").style.display="";
-}
